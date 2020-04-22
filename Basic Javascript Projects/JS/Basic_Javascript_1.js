7b555091f8a46bf29cea893438b67da669599d25
KevinFrankhouser <62575473+KevinFrankhouser@users.noreply.github.com>
2020-04-22 00:01:06 -0700

    initial upload
    First upload of JS files

---
+++ Basic Javascript Projects/JS/Basic_Javascript_1.js
@@ -0,0 +1,14 @@
+window.alert("Welcome to my first bit" 
++" of Javascript code. It's been great"
++" learning this language so far");
+
+var firstTwin = "Seth", secondTwin = "Skyler", youngestKid = "Jackson", parentA = "Amanda", parentB = "Kevin";
+firstTwin = firstTwin.fontcolor("green");
+secondTwin = secondTwin.fontcolor("blue");
+youngestKid = youngestKid.fontcolor("red");
+parentA = parentA.fontcolor("purple");
+parentB = parentB.fontcolor("orange");
+
+document.write("Here is another bit of JS code! ");
+
+document.write(17 + 16);
