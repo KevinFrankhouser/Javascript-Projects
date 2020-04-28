63effdb0168e111b2d97e1cea4478edaf6ec42cc
KevinFrankhouser <62575473+KevinFrankhouser@users.noreply.github.com>
2020-04-28 01:13:35 -0700

    update js
    Updated files with different math functions and methods.

---
+++ Basic Javascript Projects/JS/Basic_Javascript_3.js
@@ -0,0 +1,36 @@
+function addition1() {
+    var simple_add = 5 + 5;
+    document.getElementById("Math_Add").innerHTML = "The answer is " + simple_add;
+}
+
+function subtract1() {
+    var simple_sub = 5 - 5;
+    document.getElementById("Math_Sub").innerHTML = "The answer is " + simple_sub;
+}
+
+function multiply1() {
+    var simple_multi = 5 * 5;
+    document.getElementById("Math_Multi").innerHTML = "The answer is " + simple_multi;
+}
+
+function divide1() {
+    var simple_div = 5 / 5;
+    document.getElementById("Math_Div").innerHTML = "The answer is " + simple_div;
+}
+
+function modulus1() {
+    var simple_mod = 19 % 4;
+    document.getElementById("Math_Mod").innerHTML = "The answer is " + simple_mod;
+}
+
+window.alert(Math.random() * 1000)
+
+function inc1() {
+    var simple_add = 5 + 5;
+    document.getElementById("Math_Add").innerHTML = "The answer is " + simple_add;
+}
+
+function dec1() {
+    var simple_add = 5 + 5;
+    document.getElementById("Math_Add").innerHTML = "The answer is " + simple_add;
+}
