54c176e7319e77b598fa65d535288c2c61a60907
KevinFrankhouser <62575473+KevinFrankhouser@users.noreply.github.com>
2020-04-28 13:05:51 -0700

    update js operators
    Updated with use of the following operators: AND, NOT, OR, ==, ===, <, >

---
+++ Basic Javascript Projects/JS/Basic_Javascript_5.js
@@ -0,0 +1,28 @@
+document.write(typeof "this type");
+
+document.write("<br>")
+
+document.write("1016" + 1986);
+
+document.write("<br>")
+
+x = 5
+y = "5"
+document.write(x == y);
+
+document.write("<br>")
+
+document.write(x === y);
+
+document.write("<br>")
+
+document.write(13 > 10 || 6 < 5);
+
+document.write("<br>")
+
+document.write(4 > 2 && 21 < 38);
+
+document.write("<br>")
+function not_Function() {
+    document.getElementById("Not").innerHTML = !(20 > 10);
+}
