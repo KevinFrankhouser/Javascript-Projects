57436e28eaf74e684617a2306447e69f9239cd9c
KevinFrankhouser <62575473+KevinFrankhouser@users.noreply.github.com>
2020-04-27 18:23:30 -0700

    update js project
    Changes showing concatenation in JS and utilizing getElementById function

---
+++ Basic Javascript Projects/JS/Basic_Javascript_2.js
@@ -0,0 +1,5 @@
+function Function1() {
+var sentence1 = "This is the first and";
+sentence1 += " second part of my sentence put together"
+document.getElementById("concat").innerHTML = sentence1;
+}
